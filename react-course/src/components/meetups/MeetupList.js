f2a03ad87b39c672399b28f4e6019be45d681c16
fullstackdev710 <fullstack710dev@gmail.com>
2023-03-06 23:43:48 +0800

    Getting Started with Fetching Data

--- react-course/src/components/meetups/MeetupList.js
+++ react-course/src/components/meetups/MeetupList.js
@@ -8,8 +8,8 @@ function MeetupList(props) {
     <ul className={classes.list}>
       {props.meetups.map((meetup) => (
         <MeetupItem
-          key={meetup.id}
-          id={meetup.id}
+          // key={meetup.id}
+          // id={meetup.id}
           image={meetup.image}
           title={meetup.title}
           address={meetup.address}
