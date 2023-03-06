34d9f8a9406ffa6f9abca109bb2946ae8c06e211
fullstackdev710 <fullstack710dev@gmail.com>
2023-03-07 00:00:42 +0800

    Using the "useEffect" Hook

--- react-course/src/pages/AllMeetups.js
+++ react-course/src/pages/AllMeetups.js
@@ -1,20 +1,31 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(false);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch(
-    "https://react-getting-started-ad6b7-default-rtdb.firebaseio.com/meetups.json"
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-ad6b7-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
   if (isLoading) {
     return (
