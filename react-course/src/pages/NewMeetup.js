a7c28c06dec26258f1cc812a648d0fe8e5b4351d
fullstackdev710 <fullstack710dev@gmail.com>
2023-03-06 13:13:01 +0800

    Preparing The App For Http Requests & Adding a...

--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -3,10 +3,11 @@ import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function addMeetupHandler() {}
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
