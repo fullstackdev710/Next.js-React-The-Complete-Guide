8c25ad12a77669f556d77c2cd76e99f08cdee446
fullstackdev710 <fullstack710dev@gmail.com>
2023-03-09 03:29:29 +0800

    Adding Regular React Components

--- nextjs-course/pages/index.js
+++ nextjs-course/pages/index.js
@@ -1,7 +1,10 @@
+import { getFeaturedEvents } from "../dummy-data";
+
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>Home Page</h1>
+      <ul></ul>
     </div>
   );
 }
