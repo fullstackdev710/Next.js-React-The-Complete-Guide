d6a9d1e5ca7e78f792c4ffb62f76b3600a73e5e7
fullstackdev710 <fullstack710dev@gmail.com>
2023-03-08 10:24:00 +0800

    Building Nested Dynamic Routes & Paths

---
+++ nextjs-course/pages/clients/[id]/index.js
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+function ClientProjectPage() {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>The Projects of a Given</h1>
+    </div>
+  );
+}
+
+export default ClientProjectPage;
