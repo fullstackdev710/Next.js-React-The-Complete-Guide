416fcb7acecd55eaceff525a8e65839a1fdb6d37
fullstackdev710 <fullstack710dev@gmail.com>
2023-03-11 10:21:59 +0800

    Initialize-Adding Backend Code with API Routes(Fullstack React)

--- nextjs-course/pages/index.js
+++ nextjs-course/pages/index.js
@@ -1,23 +1,9 @@
-import { getFeaturedEvents } from "../helpers/api-util";
-import EventList from "../components/events/event-list";
-
-function HomePage(props) {
+function HomePage() {
   return (
     <div>
-      <EventList items={props.events} />
+      <h1>The Home Page</h1>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const featuredEvents = await getFeaturedEvents();
-
-  return {
-    props: {
-      events: featuredEvents,
-    },
-    revalidate: 1800,
-  };
 }
 
 export default HomePage;
