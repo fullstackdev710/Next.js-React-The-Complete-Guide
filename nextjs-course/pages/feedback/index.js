634d5c5ca41e2028064d507ce400f2a8f760ae0e
fullstackdev710 <fullstack710dev@gmail.com>
2023-03-11 22:44:21 +0800

    Using API Routes For Pre-Rendering Pages

---
+++ nextjs-course/pages/feedback/index.js
@@ -0,0 +1,26 @@
+import { buildFeedbackPath, extractFeedback } from "../api/feedback";
+
+function FeedbackPage(props) {
+  return (
+    <div>
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+}
+
+export default FeedbackPage;
