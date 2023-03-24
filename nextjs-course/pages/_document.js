5f7adb1ca4b3a2234ce9f5f4d50bb55275e75ac8
fullstackdev710 <fullstack710dev@gmail.com>
2023-03-24 09:52:47 +0800

    Adding a _document.js File

---
+++ nextjs-course/pages/_document.js
@@ -0,0 +1,15 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocumnt extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
