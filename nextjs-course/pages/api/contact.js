8b7341376164d5c037ae1fc692f382e44bf0351d
fullstackdev710 <fullstack710dev@gmail.com>
2023-03-24 11:48:36 +0800

    The NextJS Config File & Working With Environment Variables

--- nextjs-course/pages/api/contact.js
+++ nextjs-course/pages/api/contact.js
@@ -24,10 +24,10 @@ async function handler(req, res) {
     };
 
     let client;
+
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.px775xo.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
     try {
-      client = await MongoClient.connect(
-        "mongodb+srv://fullstack710dev:XQi3WUJOTAVBijPm@cluster0.px775xo.mongodb.net/my-site?retryWrites=true&w=majority"
-      );
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       res.status(500).json({ message: "Could not connect to database." });
       return;
